92096972273f2d7d35531fb45da88c5a246187c1
antgit <iljinaa@gmail.com>
2016-02-17 19:37:06 +0200

    Move

---
+++ src/RestaurantsManager/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Mvc;
+using RestaurantsManager.Repositories;
+
+namespace RestaurantsManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly RestaurantRepository _restaurantRepository;
+
+        public HomeController(RestaurantRepository restaurantRepository)
+        {
+            _restaurantRepository = restaurantRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- src/RestaurantsManager/Startup.cs
+++ src/RestaurantsManager/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using RestaurantsManager.Repositories;
 
 namespace RestaurantsManager
 {
@@ -11,6 +12,8 @@ namespace RestaurantsManager
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddTransient<RestaurantRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
